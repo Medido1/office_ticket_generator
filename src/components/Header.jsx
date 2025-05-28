9630afc7981d09f359fe6487bf62bedd20e8ac0e
Medido1 <medi1222@gmail.com>
2025-05-28 18:24:24 +0100

    Create website header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="p-4 bg-blue-300 flex justify-center items-center w-full shadow-md">
+      <h1 className="text-2xl font-bold">
+        Ticket Generator
+      </h1>
+    </header>
+  )
+}
+
+export default Header;
