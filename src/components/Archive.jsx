5a6ddcb87fb8a560d0440c93139a4b12f29128fe
Medido1 <medi1222@gmail.com>
2025-06-05 19:10:18 +0100

    Add class for button style to avoid repetition

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -1,33 +1,29 @@
 import { Link } from "react-router-dom";
 
 function Archive() {
+  const buttonStyle = `mt-4 px-8 py-2 rounded-full cursor-pointer border-black border-2
+            bg-blue-200 font-bold hover:bg-blue-100 hover:scale-125 transition duration-100`
   return (
     <div className="flex flex-col justify-center items-center gap-4 bg-white p-2 shadow-md
       rounded-lg w-[25%]">
       <Link to='/anapath'>
         <button
-          className="mt-4 px-8 py-2 rounded-full cursor-pointer border-black border-2
-            bg-blue-200 font-bold hover:bg-blue-100 hover:scale-125 transition duration-100">
+          className={buttonStyle}>
           Anapath
         </button>
       </Link>
       <Link to="/cytoponction">
         <button
-          className="mt-4 px-8 py-2 rounded-full cursor-pointer border-black border-2
-            bg-blue-200 font-bold hover:bg-blue-100 hover:scale-125 transition duration-100">
+          className={buttonStyle}>
           Cytoponction
         </button>
       </Link>
       <Link to="/fcv">
         <button
-          className="mt-4 px-8 py-2 rounded-full cursor-pointer border-black border-2
-            bg-blue-200 font-bold hover:bg-blue-100 hover:scale-125 transition duration-100">
+          className={buttonStyle}>
           F.C.V
         </button>
       </Link>
-      <button>
-
-      </button>
     </div>
   )
 }
