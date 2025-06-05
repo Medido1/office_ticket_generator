5f1c2547ff8d8f43023ab51115ea7bf7be1ae2e1
Medido1 <medi1222@gmail.com>
2025-06-05 19:14:02 +0100

    Add icons and h2

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { FaFolderOpen } from "react-icons/fa";
 
 function Archive() {
   const buttonStyle = `mt-4 px-8 py-2 rounded-full cursor-pointer border-black border-2
@@ -6,22 +7,25 @@ function Archive() {
   return (
     <div className="flex flex-col justify-center items-center gap-4 bg-white p-2 shadow-md
       rounded-lg w-[25%]">
+      <h2 className="text-center text-xl font-bold">
+        Archive d'analyses
+      </h2>
       <Link to='/anapath'>
         <button
           className={buttonStyle}>
-          Anapath
+          <FaFolderOpen className="inline-block mr-2 mb-1" />Anapath
         </button>
       </Link>
       <Link to="/cytoponction">
         <button
           className={buttonStyle}>
-          Cytoponction
+          <FaFolderOpen className="inline-block mr-2 mb-1" />Cytoponction
         </button>
       </Link>
       <Link to="/fcv">
         <button
           className={buttonStyle}>
-          F.C.V
+          <FaFolderOpen className="inline-block mr-2 mb-1" />F.C.V
         </button>
       </Link>
     </div>
