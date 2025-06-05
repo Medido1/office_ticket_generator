944a74ea101006d562b8f0a54442049bf1f2116d
Medido1 <medi1222@gmail.com>
2025-06-05 18:45:22 +0100

    Implement lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import './App.css'
 import Header from "./components/Header";
 import Main from "./components/Main";
-import Anapath from './components/Anapath';
-import Cytoponction from './components/Cytoponction';
-import FCV from './components/Fcv';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotFound from './components/NotFound';
+import { lazy, Suspense } from 'react';
+
+const Anapath = lazy(() => import('./components/Anapath'));
+const Cytoponction = lazy(() => import('./components/Cytoponction'));
+const FCV = lazy(() => import('./components/Fcv'));
 
 function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Main />}/>
-        <Route path="/anapath" element={<Anapath />}/>
-        <Route path="/cytoponction" element={<Cytoponction />}/>
-        <Route path="/fcv" element={<FCV />}/>
-        <Route path='*' element={<NotFound />}/>
-      </Routes>
+      <Suspense fallback={<div className='text-center text-2xl'>Loading ...</div>}>
+        <Routes>
+          <Route path="/" element={<Main />}/>
+          <Route path="/anapath" element={<Anapath />}/>
+          <Route path="/cytoponction" element={<Cytoponction />}/>
+          <Route path="/fcv" element={<FCV />}/>
+          <Route path='*' element={<NotFound />}/>
+        </Routes>
+      </Suspense>
     </Router> 
   )
 }
