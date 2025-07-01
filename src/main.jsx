5d96c409f19f5443ade1d48eaafda9ecae472a71
Medido1 <medi1222@gmail.com>
2025-07-01 18:21:45 +0100

    Enable offline support with PWA

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {GlobalProvider} from "./context/GlobalContext.jsx";
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      navigateFallback: '/index.html',
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      includeAssets: ['microscope.png'],
+      manifest: {
+        name: "Ticket Generator",
+        start_url: '/',
+        display: "browser",
+        background_color: '#ffffff',
+        theme_color: '#317EFB',
+      }
+    })
+  ],
 })
